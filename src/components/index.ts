1a87e50de75d1d1667b837c3176ee8be186b61a0
Fiqri Syah Redha <wibulo27@gmail.com>
2023-08-27 14:36:27 +0700

    add select component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,9 @@
 export { default as Button } from "./button";
 export { default as Input } from "./input";
+export { default as Select } from "./select";
 export { default as Spinner } from "./spinner";
+
+export * from "./button";
+export * from "./select";
+
 export { default as PokemonCard } from "./pokemon-card";
